27afba7a115ab056025697dd86e97e06170a9770
Borcze Angelov <Borcze.Angelov@yahoo.com>
2023-02-18 13:46:42 +0100

    add routing for new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-crypto-market';
+  selectedCurrency: string = "USD";
+
+  constructor() {
+
+  }
+
+  sendCurrency(event: string) {
+    console.log(event);
+  }
 }
